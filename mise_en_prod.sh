d50df475b8c42523ee1424b67df9f8e166620818
Cedric Temple <cedrictemple@cedrictemple.info>
2021-06-09 17:27:13 +0200

    MAJ des dépendances + Jekyll + fix MEP

--- mise_en_prod.sh
+++ mise_en_prod.sh
@@ -2,4 +2,4 @@
 
 pkill jekyll
 sleep 2
-JEKYLL_ENV=production jekyll build && scp -r _site root@cedrictemple.net:/var/www/blog.cedrictemple.net/
+JEKYLL_ENV=production bundle exec jekyll build && scp -r _site root@cedrictemple.net:/var/www/blog.cedrictemple.net/
