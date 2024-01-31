9f6322a6502260a9529fb5ad264071dedd2864de
Cedric Temple <cedrictemple@cedrictemple.info>
2024-01-31 08:32:26 +0100

    Activation mise en prod

--- podman_mise_en_prod.sh
+++ podman_mise_en_prod.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 podman container list | grep -v CONTAINER | awk '{print $1;}' | while read a; do podman stop $a; done
 podman run --rm -it --network host -v "/home/cedric/GIT/blog.cedrictemple.net:/srv/jekyll" -e JEKYLL_ROOTLESS=1 -e BUNDLE_APP_CONFIG='.bundle' jekyll/jekyll:stable bash ./jekyll_build_for_prod.sh
-#scp -r ~/GIT/blog.cedrictemple.net/_site root@cedrictemple.net:/var/www/blog.cedrictemple.net/
+scp -r ~/GIT/blog.cedrictemple.net/_site root@cedrictemple.net:/var/www/blog.cedrictemple.net/
